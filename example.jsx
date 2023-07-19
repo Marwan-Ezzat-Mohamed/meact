e9f9c3bc5176e1e56686492d2c3680af590ade61
Marwan-Ezzat-Mohamed <marwan.ezzat.badawi@gmail.com>
2023-07-19 20:46:53 +0300

    feat : update example app

--- example.jsx
+++ example.jsx
@@ -1,20 +1,45 @@
 import Meact from "./index.js";
+import "./example.css";
 
 function App() {
   const [counter, setCounter] = Meact.useState(1);
+  const [name, setName] = Meact.useState("");
 
   return (
-    <div style="display: flex; flex-direction: row;">
-      <button
-        onClick={() => {
-          setCounter((old) => {
-            // console.log("first");
-            return old + 1;
+    <div className="App">
+      <h1>Meact</h1>
+      <input
+        type="text"
+        placeholder="Enter your name"
+        value={name}
+        onInput={(e) => {
+          setName(() => {
+            return e.target.value;
           });
         }}
-      >
-        {counter}
-      </button>
+      />
+      <h2>Hello {name}</h2>
+      <div>
+        <button
+          onClick={() => {
+            setCounter((old) => {
+              // console.log("first");
+              return old + 1;
+            });
+          }}
+        >
+          You clicked {counter} times
+        </button>
+        <div className="items">
+          {[1, 2, 3].map((item) => {
+            return (
+              <div key={item}>
+                <h2>Item: {item}</h2>
+              </div>
+            );
+          })}
+        </div>
+      </div>
     </div>
   );
 }
